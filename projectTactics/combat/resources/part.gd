20e6951d2f29cdc6f22f4a6cc469526e0a1bf5d2
Jayden <everesja@gmail.com>
2024-03-28 20:10:59 +1300

    Added part item resource

---
+++ projectTactics/combat/resources/part.gd
@@ -0,0 +1,27 @@
+@tool
+extends Resource
+class_name Part
+
+@export var name : String = ""
+@export_enum("Arm", "Leg", "Chest", "Core", "Head") var type = 0
+@export var model : Mesh
+
+@export_subgroup("Flavour Text")
+@export_multiline var description : String = ""
+
+@export_subgroup("Durability")
+@export_range(0, 10000) var maxDurability : int: 
+	get: return maxDurability;
+	set(newValue):
+		maxDurability = newValue
+		currentDurability = newValue
+@export var currentDurability : int:
+	get: return currentDurability;
+	set(newValue): currentDurability = clamp(newValue, 0, maxDurability);
+	
+@export_subgroup("Combat")
+@export var damage : int = 0
+@export var armorRating : int = 0
+@export var speedRating : int = 0
+@export_range(0, 10000) var range : int = 0
+@export_range(0, 10000) var splash : int = 0
--- projectTactics/hubs/player/scripts/item.gd
+++ projectTactics/hubs/player/scripts/item.gd
@@ -1,10 +1,34 @@
+@tool
 extends Node
 
 const INTERACTION_TYPE : String = "item"
-@export_category("Item")
-@export_subgroup("Flavour")
-@export var itemName : String = ""
-@export var description : String = ""
-@export_subgroup("Traits")
-@export var armor : int = 0
-@export var effects : Array[int] = [] 
+@export_category("Item Data")
+@export_enum("Part", "Misc") var itemType : int = 0 :
+	set(value):
+		itemType = value
+		notify_property_list_changed()
+		refreshItem()
+@export var part : Part : 
+	set(value):
+		part = value
+		refreshItem()
+@export var misc : int
+
+@export var refresh : bool = false :
+	set = refreshItem
+
+func _validate_property(property: Dictionary):
+	if property.name == "part" and itemType != 0:
+		property.usage = PROPERTY_USAGE_NO_EDITOR
+	elif property.name == "misc" and itemType != 1:
+		property.usage = PROPERTY_USAGE_NO_EDITOR
+
+func refreshItem(_refreshValue = false):
+	if itemType == 0:
+		if part != null:
+			if part.model != null:
+				$mesh.mesh = part.model
+				$hitbox.shape = $mesh.mesh.create_trimesh_shape()
+				return
+		$mesh.mesh = BoxMesh.new()
+		$hitbox.shape = $mesh.mesh.create_trimesh_shape()
